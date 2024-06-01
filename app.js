022bbcbb6a7e1ebc50523805b3418b1d9fa92141
Laxman Choudhary <55774606+laxman989@users.noreply.github.com>
2024-06-01 23:39:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -41,9 +41,9 @@ app.use("/api/v1", (req, res) => {
     })
 })
 
-app.use(express.static(path.join(__dirname,"/frontend/build")))
+app.use(express.static(path.join(__dirname,"/frontend/public")))
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,"/frontend/build/index.html"));
+    res.sendFile(path.resolve(__dirname,"/frontend/public/index.html"));
 })
 
 // middleware for errors
